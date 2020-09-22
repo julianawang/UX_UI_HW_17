c393eb0ccdab397abef78c1b6353fdc29d8ae3b7
julianawang <jwang16820@gmail.com>
2020-09-21 17:55:17 -0700

    Started Discovery Arts case study page

--- js/home.js
+++ js/home.js
@@ -40,3 +40,13 @@ document.addEventListener('DOMContentLoaded',function(event){
   // start the text animation
   StartTextAnimation(0);
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
